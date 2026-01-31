aa0fd6a531c11e2ae3b81348efdf4c2aa0d2a05c
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2026-01-31 09:48:25 +0530

    seperate the tiptap css. and back to react 18.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./theme.css";
+import "./styles/index.css";
+import "./styles/theme.css";
+import "./styles/tiptap.css"
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { HelmetProvider } from "react-helmet-async";
