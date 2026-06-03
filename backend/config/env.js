7955bf2489b05a490dd69fc1c58ab865dd9c747b
Abhijeet Singh Rajput <134230962+abhijeetSinghRajput@users.noreply.github.com>
2026-06-03 09:43:27 +0530

    Update env.js
    add backend url

--- backend/config/env.js
+++ backend/config/env.js
@@ -14,6 +14,7 @@ export const ENV = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:5000",
 
 
   JWT_SECRET: required("JWT_SECRET"),
